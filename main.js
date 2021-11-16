38976667e0c1e6e28016e09348c36d1e96683b2a
subins1106 <munsb2002@naver.com>
2021-11-16 15:20:36 +0900

    위치 조정

--- main.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn =document.querySelector('.navbar__toogleBtn');
-const right = document.querySelector('.right');
-
-toggleBtn.addEventListener('click', ()=> {
-  right.classList.toggle('active');
-
-});
