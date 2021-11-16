176291abd25e2818f458c36cb0dea93689088014
subins1106 <munsb2002@naver.com>
2021-11-17 03:23:18 +0900

    반응형 웹 추가

---
+++ main.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.querySelector(".fl_toggleBtn");
+const menu = document.querySelector(".fl_right");
+
+
+toggleBtn.addEventListener("click", () => {
+  right.classList.toggle("active");
+});
