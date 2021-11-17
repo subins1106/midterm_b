89f44de4fefc27fa0d8c8fb9c26288b17621002c
subins1106 <munsb2002@naver.com>
2021-11-18 00:07:19 +0900

    연결 문제와 반응형웹 색 변경

--- main.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelector(".fl_toggleBtn");
-const menu = document.querySelector(".fl_right");
-
-
-toggleBtn.addEventListener("click", () => {
-  right.classList.toggle("active");
-});
